03c893bcec437424c96b0e2c7139be270edd2bd7
victorceschini <113563050+victorceschini@users.noreply.github.com>
2024-01-19 00:31:36 -0300

    Emissor.java

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Emissor.java
@@ -28,7 +28,7 @@ public class Emissor
 
     while(true)
     {
-        
+        // Caso ja tenha um usuario "@nome"
         if(QUEUE_NAME != "")
         {
             System.out.print(remetente + ">> ");
@@ -37,6 +37,7 @@ public class Emissor
 
             String message_to_be_sent = sc.nextLine();
             
+            // verifica se o usuario deseja mudar o remetente
             if(message_to_be_sent.startsWith("@"))
             {
                 remetente = message_to_be_sent;
@@ -44,14 +45,15 @@ public class Emissor
                 continue;
             }
 
+            // envio da mensagem
             String final_message = usuario + " diz: " + message_to_be_sent;
             channel.basicPublish("",       QUEUE_NAME, null,  final_message.getBytes("UTF-8"));
         }
         else
         {
             System.out.print(">> ");
-            
             remetente = sc.nextLine();
+            // So ira sair do ">> " quando for inserido um usuario com "@" no inicio
             if(remetente.startsWith("@"))
             {
                 QUEUE_NAME = remetente.substring(1);
