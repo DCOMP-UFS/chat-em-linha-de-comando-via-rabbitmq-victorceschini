96896a9547e5ff2cb619065fbf11a807f4f037ff
victorceschini <113563050+victorceschini@users.noreply.github.com>
2024-01-19 00:25:00 -0300

    Add files via upload

---
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Emissor.java
@@ -0,0 +1,62 @@
+package br.ufs.dcomp.ChatRabbitMQ;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Emissor
+{
+  public static void main(String[] argv) throws Exception 
+  {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("54.165.215.142"); // Alterar
+    factory.setUsername("admin"); // Alterar
+    factory.setPassword("awspass123"); // Alterar
+    factory.setVirtualHost("/");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+    
+    System.out.print("User: ");
+    Scanner sc = new Scanner(System.in);
+    String usuario = sc.nextLine();
+                      //(queue-name, durable, exclusive, auto-delete, params); 
+    channel.queueDeclare(usuario, false,   false,     false,       null);
+    
+    String QUEUE_NAME = "";
+    String remetente = "";
+
+    while(true)
+    {
+        
+        if(QUEUE_NAME != "")
+        {
+            System.out.print(remetente + ">> ");
+
+            channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+
+            String message_to_be_sent = sc.nextLine();
+            
+            if(message_to_be_sent.startsWith("@"))
+            {
+                remetente = message_to_be_sent;
+                QUEUE_NAME = remetente.substring(1);
+                continue;
+            }
+
+            String final_message = usuario + " diz: " + message_to_be_sent;
+            channel.basicPublish("",       QUEUE_NAME, null,  final_message.getBytes("UTF-8"));
+        }
+        else
+        {
+            System.out.print(">> ");
+            
+            remetente = sc.nextLine();
+            if(remetente.startsWith("@"))
+            {
+                QUEUE_NAME = remetente.substring(1);
+            }
+        }
+    }
+  }
+}
