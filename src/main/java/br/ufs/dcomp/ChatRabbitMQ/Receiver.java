95caa58f9e573bd83345be881ef3810ede247e7a
victorceschini <113563050+victorceschini@users.noreply.github.com>
2024-02-03 17:42:19 -0300

    Mudanças para ser compatível com ProtocolBuffer

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Receiver.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Receiver.java
@@ -5,16 +5,28 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import com.google.protobuf.ByteString;
+
 
 public class Receiver implements Runnable
 {
     Channel channel;
     String usuario;
+    String emissor;
+    String data;
+    String hora;
+    String grupo;
+    String mensagem_recebida = "";
 
     public Receiver(Channel channel, String usuario)
     {
         this.channel = channel;
         this.usuario = usuario;
+        emissor = "";
+        data = "";
+        hora = "";
+        mensagem_recebida = "";
+        grupo = "";
     }
     
     @Override
@@ -27,21 +39,26 @@ public class Receiver implements Runnable
             {
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException
                 {
-                    String message = new String(body, "UTF-8");
-
-                    // Obtém a data atual
-                    Date dataAtual = new Date();
-
-                    // Define o formato desejado
-                    SimpleDateFormat formato = new SimpleDateFormat("(dd/MM/yyyy 'às' HH:mm)");
-
-                    // Formata a data para o formato desejado
-                    String dataFormatada = formato.format(dataAtual);
-
-                    System.out.println('\n' + dataFormatada + " " + message);
+                    MensagemProto.Mensagem mensagem = MensagemProto.Mensagem.parseFrom(body);
                     
-                    // Imprime de volta a parte para enviar mensagem para um remetente
-                    System.out.print(Chat.getRemetente() + ">> ");
+                    // Extraindo dados da mensagem
+                    emissor = mensagem.getEmissor();
+                    if(!emissor.equals(usuario))
+                    {
+                       data = mensagem.getData();
+                        hora = mensagem.getHora();
+                        grupo = mensagem.getGrupo();
+                        MensagemProto.Conteudo conteudo = mensagem.getConteudo();
+                    
+                        mensagem_recebida = conteudo.getCorpo().toStringUtf8();
+                    
+                        System.out.print("\n(" + data + " às " + hora + ") ");
+                        System.out.println(emissor + grupo + " diz: " + mensagem_recebida);
+
+
+                        // Imprime de volta a parte para enviar mensagem para um remetente
+                        System.out.print(Chat.getRemetente() + ">> "); 
+                    }
                 }
             };
             
